72d5d3e78eccd96be7d429b0995341d6ded553e8
YassineElkefi <yassine.elkefi6@gmail.com>
2025-10-22 12:13:14 +0100

    Initial Commit

---
+++ DriveTrack/DriveTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveTrackApp.swift
+//  DriveTrack
+//
+//  Created by Yassine EL KEFI on 22/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct DriveTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
